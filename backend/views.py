25a7e16193157a1f28184a5932bc27623eb46d84
Chris McCoy <chris@brownbagcode.com>
2011-08-24 21:11:47 -0400

    stubbing out some api calls

--- backend/views.py
+++ backend/views.py
@@ -1,5 +1,36 @@
+import json
 from django.http import HttpResponse
+from django.conf import settings
 # Create your views here.
 
 def home(request):
     return HttpResponse("woot")
+
+def add_song(requets):
+    if request.method == 'POST':
+        return HttpResponse("YES")
+    else:
+        return HttpResponse("NO")
+
+
+
+# anything that is called from /api/
+# /api/get/
+def api_get_music_home(request):
+    if request.method == 'GET':
+        return HttpResponse(json.dumps({"music_home": settings.MUSIC_HOME}))
+    else:
+        return HttpResponse("NO")
+
+#/api/post/
+def api_post_add_song(request):
+    if request.method == 'POST':
+        print request
+        return HttpResponse("OK")
+    else:
+        return HttpResponse("NO")
+#/api/put/
+
+#/api/delete/
+
+
