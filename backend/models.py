f6fd798bcdff9656ec4d2c19c955d4398ef58d91
Chris McCoy <chris@brownbagcode.com>
2011-08-24 21:12:03 -0400

    fixing models and hiding some for now

--- backend/models.py
+++ backend/models.py
@@ -1,8 +1,17 @@
 from django.db import models
+from django.contrib.auth import models as auth_models
 
 class Artist(models.Model):
     name = models.CharField(max_length=255)
     mb_id = models.CharField(max_length=40, primary_key=True)
+
+    def __unicode__(self):
+        return self.name
+
+class Album(models.Model):
+    name = models.CharField(max_length=255)
+    mb_id = models.CharField(max_length=40, primary_key=True)
+    artist_id = models.ManyToManyField(Artist)
 
     def __unicode__(self):
         return self.name
@@ -16,24 +25,17 @@ class Song(models.Model):
     def __unicode__(self):
         return self.name
 
-class Album(models.Model):
-    name = models.CharField(max_length=255)
-    mb_id = models.CharField(max_length=40), primary_key=True)
-    artist_id = models.ManyToManyField(Artist)
 
-    def __unicode__(self):
-        return self.name
-
-class CustomUser(User):
-    plays = models.IntegerField()
-
-class Play(User):
-    user = models.ForeignKey(User)
-    song = models.ForeignKey(Song)
-    play_date = models.DateTimeField()
-
-    def __unicode__(self):
-        return self.user
-
-    class Meta:
-        order_with_respect_to = 'play_date'
+#class CustomUser(auth_models.User):
+#    plays = models.IntegerField()
+#
+#class Play(self.CustomUser):
+#    user = models.ForeignKey(CustomUser)
+#    song = models.ForeignKey(Song)
+#    play_date = models.DateTimeField()
+#
+#    def __unicode__(self):
+#        return self.user
+#
+#    class Meta:
+#        order_with_respect_to = 'play_date'
